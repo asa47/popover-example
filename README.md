1c6b75345d495167a313556041b5fe2e7e1fefd3
asa47 <asa47@hotmail.it>
2021-09-09 12:35:09 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# popover-example
+## Setup
+
+The following steps will allow you to build the project and run it in your web browser:
+
+1. Install the dependencies: `npm i`
+1. Start the webserver: `ionic serve`
+1. Create the dist folder: `ionic build`
+1. Add the iOS project: `ionic capacitor add ios`
+1. Push the dist folder to the iOS project: `npx cap sync`
+1. Open XCode and run the app on the device
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,48 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+    <ion-content class="ion-padding">
+      <ion-button @click="openPopover">Open Popover</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonPage, IonButton, popoverController } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import Popover from './Popover.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     IonContent,
-    IonHeader,
     IonPage,
-    IonTitle,
-    IonToolbar
+    IonButton
+  },
+  setup() {
+    async function openPopover(ev: Event) {
+      const popover = await popoverController
+        .create({
+          component: Popover,
+          cssClass: 'popover-class',
+          event: ev,
+          translucent: false,
+          showBackdrop: true,
+          backdropDismiss: true
+        });
+      await popover.present();
+
+      const { role } = await popover.onDidDismiss();
+      console.log('onDidDismiss resolved with role', role);
+    }
+    return {
+      openPopover
+    }
   }
 });
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+ion-content {
+  --background: #ffca22;
 }
 </style>
